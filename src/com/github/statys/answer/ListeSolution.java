2bfb2ac7e81b97deb7d8f7380bfa3dab5a38aa46
Quentin Ceschin <ceschin.quentin@gmail.com>
2017-12-08 05:32:40 +0100

    Début du système de compréhension

---
+++ src/com/github/statys/answer/ListeSolution.java
@@ -0,0 +1,5 @@
+package com.github.statys.answer;
+
+public enum ListeSolution {
+	
+}
---
+++ src/com/github/statys/answer/Sujets.java
@@ -0,0 +1,5 @@
+package com.github.statys.answer;
+
+public enum Sujets {
+	git,sql,python,java,php,web,htlm,css,c ;
+}
